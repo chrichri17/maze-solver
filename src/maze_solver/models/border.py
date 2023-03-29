678e996107471ac1c06340870616dc8c6d593edf
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2023-03-30 00:34:49 +0200

    fmt

--- src/maze_solver/models/border.py
+++ src/maze_solver/models/border.py
@@ -1,12 +1,13 @@
 from enum import IntFlag, auto
 
+
 class Border(IntFlag):
     EMPTY = 0
     TOP = auto()
     BOTTOM = auto()
     LEFT = auto()
     RIGHT = auto()
-    
+
     @property
     def corner(self) -> bool:
         return self in (
@@ -15,11 +16,11 @@ class Border(IntFlag):
             self.BOTTOM | self.LEFT,
             self.BOTTOM | self.RIGHT,
         )
-    
+
     @property
     def dead_end(self) -> bool:
         return self.bit_count() == 3
-    
+
     @property
     def intersection(self) -> bool:
         return self.bit_count() < 2
--- src/maze_solver/models/role.py
+++ src/maze_solver/models/role.py
@@ -1,5 +1,6 @@
 from enum import IntEnum, auto
 
+
 class Role(IntEnum):
     NONE = 0
     ENEMY = auto()
