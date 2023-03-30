a0cc1a3261f4d1883e3524a6f558aa25a0b0c19e
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2023-03-30 02:20:03 +0200

    view: implement the decomposer

---
+++ src/maze_solver/view/decomposer.py
@@ -0,0 +1,133 @@
+from maze_solver.models.border import Border
+from maze_solver.view.primitives import (
+    DisjointLines,
+    Line,
+    NullPrimitive,
+    Point,
+    Polygon,
+    Polyline,
+    Primitive,
+)
+
+
+def decompose(border: Border, top_left: Point, square_size: int) -> Primitive:
+    top_right: Point = top_left.translate(x=square_size)
+    bottom_right: Point = top_right.translate(y=square_size)
+    bottom_left: Point = bottom_right.translate(x=-square_size)
+
+    top = Line(top_left, top_right)
+    bottom = Line(bottom_left, bottom_right)
+    left = Line(top_left, bottom_left)
+    right = Line(top_right, bottom_right)
+
+    # Square with all 4 sides
+    if border is Border.LEFT | Border.TOP | Border.RIGHT | Border.BOTTOM:
+        return Polygon(
+            [
+                top_left,
+                top_right,
+                bottom_right,
+                bottom_left,
+            ]
+        )
+
+    # Squares with 3 sides
+    if border is Border.BOTTOM | Border.LEFT | Border.TOP:
+        return Polyline(
+            [
+                bottom_right,
+                bottom_left,
+                top_left,
+                top_right,
+            ]
+        )
+
+    if border is Border.LEFT | Border.TOP | Border.RIGHT:
+        return Polyline(
+            [
+                bottom_left,
+                top_left,
+                top_right,
+                bottom_right,
+            ]
+        )
+
+    if border is Border.TOP | Border.RIGHT | Border.BOTTOM:
+        return Polyline(
+            [
+                top_left,
+                top_right,
+                bottom_right,
+                bottom_left,
+            ]
+        )
+
+    if border is Border.RIGHT | Border.BOTTOM | Border.LEFT:
+        return Polyline(
+            [
+                top_right,
+                bottom_right,
+                bottom_left,
+                top_left,
+            ]
+        )
+
+    # Squares with 2 sides: corners
+    if border is Border.LEFT | Border.TOP:
+        return Polyline(
+            [
+                bottom_left,
+                top_left,
+                top_right,
+            ]
+        )
+
+    if border is Border.TOP | Border.RIGHT:
+        return Polyline(
+            [
+                top_left,
+                top_right,
+                bottom_right,
+            ]
+        )
+
+    if border is Border.BOTTOM | Border.LEFT:
+        return Polyline(
+            [
+                bottom_right,
+                bottom_left,
+                top_left,
+            ]
+        )
+
+    if border is Border.RIGHT | Border.BOTTOM:
+        return Polyline(
+            [
+                top_right,
+                bottom_right,
+                bottom_left,
+            ]
+        )
+
+    # Squares with 2 sides: others
+    if border is Border.LEFT | Border.RIGHT:
+        return DisjointLines([left, right])
+
+    if border is Border.TOP | Border.BOTTOM:
+        return DisjointLines([top, bottom])
+
+    # Squares with 1 side
+    if border is Border.TOP:
+        return top
+
+    if border is Border.RIGHT:
+        return right
+
+    if border is Border.BOTTOM:
+        return bottom
+
+    if border is Border.LEFT:
+        return left
+
+    # Default value
+    return NullPrimitive
